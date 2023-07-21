bd7a0e658ee134136dad45e3c0f3f0d4f6220c89
mcm-ryan <mcm.ryan02@gmail.com>
2023-07-21 00:46:07 -0500

    Got the Jpa repository instance connected

--- src/main/java/com/Service/GameService.java
+++ src/main/java/com/Service/GameService.java
@@ -1,6 +1,7 @@
 package com.Service;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Entity.Game;
@@ -11,6 +12,7 @@ public class GameService {
 
     private final GameRepository gameRepository;
 
+    @Autowired
     public GameService(GameRepository givenGameRepository) {
         this.gameRepository = givenGameRepository;
     }
--- src/main/java/com/SpringBoot/DemoApplication.java
+++ src/main/java/com/SpringBoot/DemoApplication.java
@@ -3,13 +3,19 @@ package com.SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.Entity")
+@EnableJpaRepositories(basePackages = "com.Repository")
+@ComponentScan({"com.Service", "com.SpringBoot"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 
 }
