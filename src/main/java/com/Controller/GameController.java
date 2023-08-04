4b7d809b37597831f46e5453fd4f4e5f0d88cc8c
mcm-ryan <mcm.ryan02@gmail.com>
2023-08-03 19:09:53 -0500

    Beginning of random games on home page

--- src/main/java/com/Controller/GameController.java
+++ src/main/java/com/Controller/GameController.java
@@ -3,8 +3,12 @@ package com.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
 import java.util.List;
 import com.Entity.Game; 
 import com.Service.GameService;
@@ -19,9 +23,21 @@ public class GameController {
     }
 
     @GetMapping(path = "/games")
+    @PostMapping("/games")
     public String gamesPage(Model model) {
         List<Game> games = gameService.getAllGames();
         model.addAttribute("games", games);
-        return "games"; // The name of your games.html template
+        return "games"; 
     }
+
+
+    private List<Game> getRandomGamesSubset(List<Game> games) {
+        if (games.size() <= 3){
+            return games;
+        } else {
+            Collections.shuffle(games);
+            return games.subList(0, 3);
+        }
+    }
+    
 }
