c84d87815a7a92233665125dfc252449b809a342
rsaavedrajr <111775893+rsaavedrajr@users.noreply.github.com>
2023-08-10 19:19:46 -0500

    better paramed testing

--- src/test/java/com/spotCheckTest.java
+++ src/test/java/com/spotCheckTest.java
@@ -1,6 +1,8 @@
 package com;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -34,46 +36,43 @@ public class spotCheckTest {
 		assertThat(gameList).isEmpty();
 	}
 
-	@Test
-	public void doesItDetectGenre() {
-		List<Game> gameList = testrepo.findByGenre("Fighting");
-		assertFalse(gameList.isEmpty());
+	@ParameterizedTest(name = "is {0} a genre? = {1}")
+	@CsvSource({
+		"Fighting, True",
+		"Action, True",
+		"Strategy, True",
+		"Sports, True",
+		"Shooter, True",
+		"Survival, True",
+		"dertg45y6g, False",
+	})
+	public void doesItDetectGenre(String genre, boolean expected) {
+		List<Game> gameList = testrepo.findByGenre(genre);
+		assertFalse(!(gameList.isEmpty() == !expected));
+
 	}
 
-	// unfinished
-	@Test
-	public void isGameInGenre() {
-		List<Game> gameList = testrepo.findByGenre("Fighting");
+	@ParameterizedTest(name = "is {0} in {1} genre? {2}")
+	@CsvSource({
+		"Rivals of Aether, Fighting, True",
+		"DELTARUNE, Action, True",
+		"Among Us, Fighting, False",
+		"Mario Party Superstars, Party, True",
+		"Minecraft, Educational, True",
+		"Portal 2, Strategy, False",
+	})
+	public void isGameInGenre(String name, String genre, Boolean expected) {
+		List<Game> gameList = testrepo.findByGenre(genre);
 
 		boolean result = false;
 
 		for (Game game : gameList) {
-			if (game.getTitle().equals("Rivals of Aether")) {
+			if (game.getTitle().equals(name)) {
 				result = true;
 			}
 		}
-		System.out.println(result);
+		System.out.println(name + " " + result);
 
-		assertFalse(!result);
+		assertFalse(!(result == expected));
 	}
-
-	// @Test
-	// @DisplayName("Is game in genre?")
-	// void addsTwoNumbers() {
-	// 	GameService test = new GameService(testrepo);
-	// 	// assertEquals(2, GameService.getGamesByCategory(""), "1 + 1 should equal 2");
-	// }
-
-	// @ParameterizedTest(name = "is {0} in {1} genre? {2}")
-	// @CsvSource({
-	// 		"Rivals of Aether, Fighting, True",
-	// 		"DELTARUNE, Action, True",
-	// 		"Among Us, Fighting, False",
-	// 		// vv tests that break the code!!
-	// })
-	// void add(String first, String second, Boolean expectedResult) {
-	// 	GameService test = new GameService(testrepo);
-	// 	// assertEquals(expectedResult, GameService.add(first, second),
-	// 			// () -> first + " is in " + second + "should equal " + expectedResult);
-	// }
 }
